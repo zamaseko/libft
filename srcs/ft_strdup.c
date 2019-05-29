95f28fa81289f0f01e2ad8e110833b12563d2006
Zandile MASEKO <zamaseko@c4r11s10.wethinkcode.co.za>
2019-05-29 12:39:02 +0200

    Added strdup

--- srcs/ft_strdup.c
+++ srcs/ft_strdup.c
@@ -6,19 +6,45 @@
 /*   By: zamaseko <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/27 15:57:02 by zamaseko          #+#    #+#             */
-/*   Updated: 2019/05/28 18:04:37 by zamaseko         ###   ########.fr       */
+/*   Updated: 2019/05/29 12:38:20 by zamaseko         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libft.h"
+#include <string.h>
+#include <stdlib.h>
 
-char	ft_strdup(const char *s1)
+char	*ft_strdup(const char *s1)
 {
-	char *i;
-	int j;
+	char *a;
+	int i;
 
 	i = 0;
+	a = (char *)malloc(sizeof(char) * (strlen(s1) + 1));
+	if (!(a = (char *)malloc(sizeof(char) * (strlen(s1) + 1))))
+		return (0);
+	while (s1[i] != '\0')
+	{
+		a[i] = s1[i];
+		i++;
+	}
+	return (a);
+}
 
-	(!(w = (char *)(malloc (sizeof(char) * (strlen(s1) +1))))
-	return (NULL);
+void	ft_putstr(char *s)
+{
+	int i;
+
+	i = 0;
+	while (s[i])
+		{
+			ft_putstr(&s[i]);
+			i++;
+		}
+}
+int main()
+{
+	char *m;
+	m = ft_strdup("Zandi");
+	ft_putstr(m);
+	return (0);
 }
