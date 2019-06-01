8355ccfd04b620f943c0884f13b3a2953b18df63
Zandile MASEKO <zamaseko@c4r11s10.wethinkcode.co.za>
2019-06-01 17:12:02 +0200

    Added lower

--- ft_tolower.c
+++ ft_tolower.c
@@ -6,7 +6,7 @@
 /*   By: zamaseko <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/21 15:57:28 by zamaseko          #+#    #+#             */
-/*   Updated: 2019/05/27 16:27:29 by zamaseko         ###   ########.fr       */
+/*   Updated: 2019/06/01 16:59:30 by zamaseko         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,3 +19,15 @@ int	ft_tolower(int z)
 	else
 		return (z);
 }
+
+/*int main(void)
+{
+	int a;
+	int b;
+
+	a = 'Y';
+	b = tolower(a);
+	ft_putchar(b);
+	ft_putchar('\n');
+	return (0);
+}*/
