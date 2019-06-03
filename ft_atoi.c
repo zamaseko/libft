aa214a43f011a3f2565a732cca884f5ba8763a92
Zandile MASEKO <zamaseko@c4r11s10.wethinkcode.co.za>
2019-06-03 12:08:10 +0200

    Added atoi

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,7 +6,7 @@
 /*   By: zamaseko <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/27 16:19:57 by zamaseko          #+#    #+#             */
-/*   Updated: 2019/06/01 16:34:52 by zamaseko         ###   ########.fr       */
+/*   Updated: 2019/06/03 12:07:45 by zamaseko         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,36 @@
 
 int	ft_atoi(const char *str)
 {
-	return (int);;
-}
+	int i;
+	int change;
+	int res;
 
+	i = 0;
+	change = 1;
+	res = 0;
+
+	while (str[i] == ' ');
+	i++;
+	while (str[i] == '\t');
+	i++;
+	while (str[i] == '\v');
+	i++;
+	while (str[i] == '\n');
+	 if (str[i] == '-' || str[i] == '+')
+	 {
+		 change = -1;
+		 i++;
+	 }
+	return (res );
+}
+int main(void)
+{
+	int i;
+	const char j;
+
+	j = "1223";
+	i = ft_atoi(j);
+	ft_putstr(i);
+	ft_putchar('\n');
+	return (0);
+}
