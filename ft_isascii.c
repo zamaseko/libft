521b727f3f21bd9e8cb667883ec45a1fb1cf8cd2
Zandile MASEKO <zamaseko@c4r11s10.wethinkcode.co.za>
2019-06-04 17:32:47 +0200

    Added isascii

--- ft_isascii.c
+++ ft_isascii.c
@@ -6,7 +6,7 @@
 /*   By: zamaseko <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/27 16:25:00 by zamaseko          #+#    #+#             */
-/*   Updated: 2019/05/27 16:26:01 by zamaseko         ###   ########.fr       */
+/*   Updated: 2019/06/04 15:38:45 by zamaseko         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,22 @@
 
 int	ft_isascii(int c)
 {
+	if ( 0 <= c && 177 >= c)
+		return (1);
+	else 
+		return (0);
 }
 
+/*#include <stdio.h>
+int main(void)
+{
+	int a;
+	int b;
+
+	a = 178;
+	b = ft_isascii(a);
+	printf("mine: %d\n", b);
+	b = isascii(a);
+	printf("pc: %d\n" , b);
+	return (0);
+}*/
