83508aa32c415899c1c75fb42cbc3133b2c0e20d
Zandile MASEKO <zamaseko@c4r11s10.wethinkcode.co.za>
2019-06-04 17:30:57 +0200

    Added isalnum

--- ft_isalnum.c
+++ ft_isalnum.c
@@ -6,7 +6,7 @@
 /*   By: zamaseko <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/27 16:22:46 by zamaseko          #+#    #+#             */
-/*   Updated: 2019/05/27 16:24:36 by zamaseko         ###   ########.fr       */
+/*   Updated: 2019/06/04 15:50:28 by zamaseko         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,21 @@
 
 int	ft_isalnum(int c)
 {
+	if (ft_isdigit(c) || ft_isalpha(0))
+		return (1);
+	else
+		return (0);
 }
 
+/*
+#include <stdio.h>
+int main(void) 
+{
+	int a;
+	int b;
+
+	a = 'A';
+	b = ft_isalnum(a);
+	printf("%d\n", b);§
+	return (0);
+}*/
