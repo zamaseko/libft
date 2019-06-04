cdd45383db0c3042e99cd506c0023415ed730156
Zandile MASEKO <zamaseko@c4r11s10.wethinkcode.co.za>
2019-06-04 17:34:55 +0200

    Added putnbr_fd

--- ft_putnbr_fd.c
+++ ft_putnbr_fd.c
@@ -6,10 +6,25 @@
 /*   By: zamaseko <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/27 16:57:42 by zamaseko          #+#    #+#             */
-/*   Updated: 2019/05/27 16:58:10 by zamaseko         ###   ########.fr       */
+/*   Updated: 2019/06/04 12:32:41 by zamaseko         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
 void	ft_putnbr_fd(int n, int fd)
+{
+	if (n < 0)
+	{
+		ft_putchar_fd(n, 1);
+	}
+	if (n >= 10)
+	{
+		ft_putnbr_fd(n / 10, fd);
+		ft_putnbr_fd(n % 10, fd);
+	}
+	if (n < 10)
+	{
+		ft_putchar_fd(n + 48, fd);
+	}
+}
