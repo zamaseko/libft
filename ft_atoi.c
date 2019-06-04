b31d380ddd25e3f69fe49dcd7237e949fd2efb45
Zandile MASEKO <zamaseko@c4r11s10.wethinkcode.co.za>
2019-06-04 17:30:24 +0200

    Added atoi

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,7 +6,7 @@
 /*   By: zamaseko <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/27 16:19:57 by zamaseko          #+#    #+#             */
-/*   Updated: 2019/06/03 12:07:45 by zamaseko         ###   ########.fr       */
+/*   Updated: 2019/06/04 17:26:09 by zamaseko         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,35 +15,44 @@
 int	ft_atoi(const char *str)
 {
 	int i;
-	int change;
-	int res;
+	long change;
+	long res;
 
 	i = 0;
 	change = 1;
 	res = 0;
 
-	while (str[i] == ' ');
-	i++;
-	while (str[i] == '\t');
-	i++;
-	while (str[i] == '\v');
-	i++;
-	while (str[i] == '\n');
-	 if (str[i] == '-' || str[i] == '+')
+	while (str[i] == ' ' || str[i] == '\t' || str[i] == '\r' || str[i] == '\n'||
+			str[i] == '\0')
+		i++;
+
+	 while (str[i] == '-' || str[i] == '+')
+		 if (str[i] == '-')
 	 {
 		 change = -1;
 		 i++;
 	 }
+		 else if (str[i] == '+')
+		 {
+			 res = +1;
+			 i++;
+		 }
+	 while (str[i] >= '0' || str[i] <= '9')
+	 {
+	 res = ((res * change) + (str[i] - '0'));
+		 i++;
+	 }
 	return (res );
 }
-int main(void)
+
+/*int main(void)
 {
 	int i;
-	const char j;
+	const char a[50] = "1223";
 
-	j = "1223";
-	i = ft_atoi(j);
-	ft_putstr(i);
+	a = "1223";
+	i = ft_atoi(a);
+	ft_putchar(i);
 	ft_putchar('\n');
 	return (0);
-}
+}*/
