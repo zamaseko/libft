0b5b20eb570e460569ad8d3b653ca3e2eb0cf0b7
Zandile MASEKO <zamaseko@c4r11s10.wethinkcode.co.za>
2019-06-04 18:13:09 +0200

    Added memset

--- ft_memset.c
+++ ft_memset.c
@@ -6,12 +6,13 @@
 /*   By: zamaseko <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/27 15:44:29 by zamaseko          #+#    #+#             */
-/*   Updated: 2019/05/27 15:45:23 by zamaseko         ###   ########.fr       */
+/*   Updated: 2019/06/04 18:10:15 by zamaseko         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	ft_memset(void *v, int c, size_t)
+void	*ft_memset(void *b, int c, size_t len)
 {
+	char *b;
 }
