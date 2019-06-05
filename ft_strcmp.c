5b13e6af040cc70af5d267cdb9cb0e739bf26852
Zandile MASEKO <zamaseko@c4r11s7.wethinkcode.co.za>
2019-06-05 18:48:26 +0200

    Added strcmp

--- ft_strcmp.c
+++ ft_strcmp.c
@@ -6,7 +6,7 @@
 /*   By: zamaseko <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/27 16:17:30 by zamaseko          #+#    #+#             */
-/*   Updated: 2019/06/05 18:30:47 by zamaseko         ###   ########.fr       */
+/*   Updated: 2019/06/05 18:45:48 by zamaseko         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,11 +24,13 @@ int	ft_strcmp(const char *s1, const char *s2)
 		if (s1[i] == s2[i])
 		{
 			i++;
-		}
-		while  (s1[i] != '\0' && s2[i] != '\0')
-		i++;
+	 	}
+		if ((unsigned char)s1[i] > (unsigned char)s2[i])
+		return (1);
+		if ((unsigned char)s1[i] < (unsigned char)s2[i])
+			return (-1);
 	}
-		return ((unsigned char)s1[i] - (unsigned char)s2[i]);
+	return (0);
 }
 /*include <stdio.h>
 int main(void)
