b409659886f9255731bfa5a3de6a037123a64b1c
Zandile MASEKO <zamaseko@c4r14s2.wethinkcode.co.za>
2019-06-06 11:06:15 +0200

    Added strstr

--- ft_strstr.c
+++ ft_strstr.c
@@ -6,7 +6,7 @@
 /*   By: zamaseko <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/27 16:14:36 by zamaseko          #+#    #+#             */
-/*   Updated: 2019/05/31 13:38:06 by zamaseko         ###   ########.fr       */
+/*   Updated: 2019/06/06 11:03:49 by zamaseko         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,21 @@
 
 char	*ft_strstr(const char *haystack, const char *needle)
 {
+	int i;
+	int j;
+
+	i = 0;
+	if (haystack[i] == '\0' &&  needle[i] == '\0')
+	return ((char *)haystack)
 	
+	j = 0;
+	if (needle[i] != '\0')
+	{
+		if (needle[j] != '\0' && haystack[i] == needle[i])
+			j++;
+		if (needle[i] == '\0')
+			i++;
+		return ((char *)haystack)
+	}
+	return(NULL);
 }
