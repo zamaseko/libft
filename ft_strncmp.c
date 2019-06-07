7567864b4cce8d711152e89adb450ba1e1994564
Zandile MASEKO <zamaseko@c4r11s10.wethinkcode.co.za>
2019-06-07 18:44:16 +0200

    Added strncmp

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -23,18 +23,15 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 		i++;	
 	return ((unsigned char)s1[i] - (unsigned char)s2[i]);
 }*/
+	
 {
 	size_t i;
 
 	i = 0;
-	while (s1[i] != '\0' && s2[i] != '\0')
+	while (s1[i] && (n - 1) > i)
 	{
-	if(s1[i] == s2[i])
-		i++;
-	if (s1[i] != '\0' && s2[i] != '\0' && (n - 1) > i)
-		i++;
+		if (s1[i] == s2[i])
+			i++;
 	}
-		return ((unsigned char)s1[i] - (unsigned char)s2[i]);
+	return ((unsigned char)s1[i] - (unsigned char)s2[i]);
 }
-	
-
