7519e7e7418457a55213a07f4bfe17c46c1b4591
Zandile MASEKO <zamaseko@c4r11s10.wethinkcode.co.za>
2019-06-07 18:45:05 +0200

    Added libft

--- libft.h
+++ libft.h
@@ -59,7 +59,7 @@ int	ft_strncmp(const char *s1, const char *s2, size_t );
 //void	ft_strclr(char *s);
 //void	ft_striter(char *s, void (*f)(char *));
 //void	ft_striteri(char *s, void (*f)(unsigned int, char *));
-//char	*ft_strmap(char const *s, char (*f)(char));
+char	*ft_strmap(char const *s, char (*f)(char));
 //char	*ft_strmapi(char const *s, char (*f)(unsigned int, char));
 //int ft_strequ(const char *s1, char const *s2);
 //int	ft_strnequ(char const *s1, char const *s2, size_t n);
