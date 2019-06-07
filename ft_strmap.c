6d234cb88c57c8344f3005c2887deb77649db060
Zandile MASEKO <zamaseko@c4r11s10.wethinkcode.co.za>
2019-06-07 18:43:11 +0200

    Added strmap

--- ft_strmap.c
+++ ft_strmap.c
@@ -12,4 +12,20 @@
 
 #include "libft.h"
 
-char	*ft_strmap(char const *s, char(*f)(char))
+char	*ft_strmap(char const *s, char (*f)(char))
+{
+	int i;
+	char *gstr;
+
+	i = 0;
+	if (!( gstr = (char *)malloc(sizeof(char) * ft_strlen(s) + 1)))
+		return (NULL);
+	while (s[i] != '\0')
+	{
+		if (!(gstr[i] = f(s[i])))
+			return (0);
+		i++;
+	}
+	gstr[i] = '\0';
+	return (gstr);
+} 
