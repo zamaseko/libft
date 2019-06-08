11b72aad3d094022b5ffa01c56b2781b4db9120e
Zandile MASEKO <zamaseko@c4r11s10.wethinkcode.co.za>
2019-06-08 14:23:15 +0200

    Added memcpy

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -12,6 +12,21 @@
 
 #include "libft.h"
 
-voi	ft_memcpy(void *restrict, const void *restrict src, size_t n)
+void	*ft_memcpy(void *restrict dst, const void *restrict src, size_t n)
 {
+	unsigned char *source;
+	unsigned char *dest;
+	size_t i;
+
+	i = 0;
+	dest = (unsigned char *)dst;
+	source = (unsigned char *)src;
+ 	if (dest == NULL && source == NULL)
+		return (NULL);
+	while (n > i)
+	{
+		dest[i] = source[i];
+		i++;
+	}
+	return (dst);
 }
