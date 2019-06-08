459bc2a77b1a11ef67761cdbd3ec7ef3f0913e9f
Zandile MASEKO <zamaseko@c4r11s10.wethinkcode.co.za>
2019-06-08 14:23:33 +0200

    Added libft

--- libft.h
+++ libft.h
@@ -18,16 +18,16 @@
 # include <stdlib.h>
 
 int	ft_atoi(const char *str);
-//void	ft_bzero(void *s, size_t n);
+void	ft_bzero(void *s, size_t n);
 int	ft_isalpha(int c);
 int	ft_isdigit(int c);
 int	ft_isalnum(int c);
 int	ft_isascii(int c);
 int	ft_isprint(int c);
 //char	*ft_itoa(int n);
-//void	*ft_memset(void *b, int c, size_t);
-//void	*ft_memcpy(void *restrict dst, const void *restrict src, size_t n);
-//void	*ft_memccpy(void *restrict dst, const void *restrict src, int c, size_t n); 
+void	*ft_memset(void *b, int c, size_t);
+void	*ft_memcpy(void *restrict dst, const void *restrict src, size_t n);
+void	*ft_memccpy(void *restrict dst, const void *restrict src, int c, size_t n); 
 //void	*ft_memmove(oid *dst, const void *src, size_t n);
 //int	ft_memcmp(const void *s1, const void *s2, size_t n);
 //void	*ft_memchr(const void*s, int c, size_t n);
