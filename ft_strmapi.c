78a69acbb8136af66739ed1cec8a5956801cac06
Zandile MASEKO <zamaseko@c4r11s10.wethinkcode.co.za>
2019-06-13 17:52:44 +0200

    Added mapi

--- ft_strmapi.c
+++ ft_strmapi.c
@@ -6,7 +6,7 @@
 /*   By: zamaseko <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/27 16:41:50 by zamaseko          #+#    #+#             */
-/*   Updated: 2019/06/11 17:25:36 by zamaseko         ###   ########.fr       */
+/*   Updated: 2019/06/13 17:05:53 by zamaseko         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,8 @@ char *ft_strmapi(char const *s, char (*f)(unsigned int, char))
 	int i;
 	char *gstr;
 	
+	if (!s || !f)
+		return (NULL);
 	i = 0;
 	if (!( gstr = (char *)malloc(sizeof(char) * ft_strlen(s) + 1)))
 		return (NULL);
