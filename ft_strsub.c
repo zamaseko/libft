4f036ef911ac4bfb1a0958484b7700f0b9c8032e
Zandile MASEKO <zamaseko@c4r11s10.wethinkcode.co.za>
2019-06-13 10:59:53 +0200

    Added strsub

--- ft_strsub.c
+++ ft_strsub.c
@@ -6,10 +6,22 @@
 /*   By: zamaseko <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/27 16:47:21 by zamaseko          #+#    #+#             */
-/*   Updated: 2019/05/27 16:48:22 by zamaseko         ###   ########.fr       */
+/*   Updated: 2019/06/13 10:58:52 by zamaseko         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
 char	*ft_strsub(char const *s, unsigned int start, size_t len)
+{
+	unsigned int i;
+	char *fstr;
+
+	i = 0;
+	fstr = 
+	while (i < len)
+	{
+		str[i] 
+	}
+	return (fstr);
+}
