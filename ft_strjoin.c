643993bc3831ed22e3fadc0978403356efe90be1
Zandile MASEKO <zamaseko@c4r11s10.wethinkcode.co.za>
2019-06-13 17:52:06 +0200

    Added join

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -6,7 +6,7 @@
 /*   By: zamaseko <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/27 16:48:49 by zamaseko          #+#    #+#             */
-/*   Updated: 2019/06/12 17:10:52 by zamaseko         ###   ########.fr       */
+/*   Updated: 2019/06/13 17:09:12 by zamaseko         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,9 +16,11 @@ char *ft_strjoin(char const *s1, char const *s2)
 {
 	char *str;
 
+	if (!s1 || !s2)
+		return (NULL);
 	str = (char *)malloc(sizeof(char) * ft_strlen(s1) + ft_strlen(s2) + 1);
 	if (!str)
-	return (NULL);
+		return (NULL);
 	ft_strcpy(str, s1);
 	ft_strcat(str, s2);
 	return (str);
