37da6c3805abf2d3ea072241eb286223e5021e06
Zandile MASEKO <zamaseko@c4r11s10.wethinkcode.co.za>
2019-06-18 17:55:25 +0200

    Added itoa&libft

--- ft_itoa.c
+++ ft_itoa.c
@@ -6,7 +6,7 @@
 /*   By: zamaseko <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/27 16:51:21 by zamaseko          #+#    #+#             */
-/*   Updated: 2019/06/18 17:05:11 by zamaseko         ###   ########.fr       */
+/*   Updated: 2019/06/18 17:52:41 by zamaseko         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,11 +15,22 @@
 char	*ft_itoa(int n)
 {
 	char *str;
-//	int i;
-//	int res;
+	int i;
+	int res;
 
-	str = (char *)malloc(sizeof(char) * n + 1); 
+	res = 0;
+	i = ft_strlen(str) + 1;
+	str = (char *)malloc(sizeof(char) * i + 1);
 	if (!str)
 		return (NULL);
-
+	if (n < 0)
+	{
+		ft_putchar('-');
+		n = -n;
+	}
+	while (n)
+	{
+		str[i] = n % 10 + '0';
+	}
+	return (str);
 }
