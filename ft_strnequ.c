bbc668758509d57279d4a01cdf457ad2c5d1673e
Zandile MASEKO <zamaseko@c4r11s10.wethinkcode.co.za>
2019-06-24 13:55:19 +0200

    Added strnequ

--- ft_strnequ.c
+++ ft_strnequ.c
@@ -6,7 +6,7 @@
 /*   By: zamaseko <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/27 16:44:05 by zamaseko          #+#    #+#             */
-/*   Updated: 2019/06/23 13:26:46 by zamaseko         ###   ########.fr       */
+/*   Updated: 2019/06/24 11:42:51 by zamaseko         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ int			ft_strnequ(char const *s1, char const *s2, size_t n)
 	i = 0;
 	if (!s1 || !s2)
 		return (0);
-	if (ft_strcmp(s1, s2) != 0 && i < n)
+	if (ft_strncmp(s1, s2, n) != 0 && i < n)
 		return (0);
 	else
 		return (1);
