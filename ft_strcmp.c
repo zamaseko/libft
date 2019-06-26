45071cad4ff183e695d96380a2cda64d4a343245
Zandile MASEKO <zamaseko@c4r11s10.wethinkcode.co.za>
2019-06-26 17:13:30 +0200

    Added *

--- ft_strcmp.c
+++ ft_strcmp.c
@@ -6,7 +6,7 @@
 /*   By: zamaseko <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/27 16:17:30 by zamaseko          #+#    #+#             */
-/*   Updated: 2019/06/26 15:37:09 by zamaseko         ###   ########.fr       */
+/*   Updated: 2019/06/26 15:44:48 by zamaseko         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,16 +19,11 @@ int	ft_strcmp(const char *s1, const char *s2)
 	i = 0;
 	while (s1[i] != '\0' && s1[i] == s2[i])
 	{
-		if (s1[i] < s2[i])
-			return (-1);
 		if (s1[i] > s2[i])
 			return (1);
+		if (s1[i] < s2[i])
+			return (-1);
 		i++;
 	}
-	if ((unsigned char)s1[i] - (unsigned char)s2[i] > 0)
-		return (1);
-	if ((unsigned char)s1[i] - (unsigned char)s2[i] < 0)
-		return (-1);
-	else
-		return (0);
+	return ((unsigned char)s1[i] - (unsigned char)s2[i]);
 }
--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: zamaseko <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/27 16:18:39 by zamaseko          #+#    #+#             */
-/*   Updated: 2019/06/25 18:18:15 by zamaseko         ###   ########.fr       */
+/*   Updated: 2019/06/26 15:49:54 by zamaseko         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,16 +21,11 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 		return (0);
 	while (s1[i] != '\0' && s1[i] == s2[i] && n - 1 > i)
 	{
-		if (s1[i] < s2[i])
-			return (1);
 		if (s1[i] > s2[i])
+			return (1);
+		if (s1[i] < s2[i])
 			return (-1);
 		i++;
 	}
-	if ((unsigned char)s1[i] - (unsigned char)s2[i] > 0)
-		return (1);
-	if ((unsigned char)s1[i] - (unsigned char)s2[i] < 0)
-		return (-1);
-	else
-		return (0);
+	return ((unsigned char)s1[i] - (unsigned char)s2[i]);
 }
